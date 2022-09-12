ddc9e12a5560cbd27c591852d5c1b06792568171
U. I. I. Derbashi <32719218+uderbashi@users.noreply.github.com>
2022-09-12 15:41:57 +0000

    changed the name of the test files, and added a testing bash script

--- test_files/gen.py
+++ test_files/gen.py
@@ -3,19 +3,19 @@ test_files = {
 	"text" : bytes.fromhex('3243 f6a8 885a 308d 3131 98a2 e037 0734'),
 	"cipher128" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c'),
 
-	"testMin1" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1'),
-	"testAdd1" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 25'),
-	"testAdd5" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 2566 6969 ea'),
+	"Min1.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1'),
+	"Add1.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 25'),
+	"Add5.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 2566 6969 ea'),
 
-	"testFakePad1" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 2500 0069 04'),
-	"testFakePad2" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 2566 5369 01'),
+	"FakePad1.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 2500 0069 04'),
+	"FakePad2.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a1e4 2566 5369 01'),
 	
-	"testNull1" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f00 84ab 5732 baa3 910e cc87 208a de53 a111'),
-	"testNull2" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f00 84ab 5732 baa3 910e cc87 208a de53 0003'),
-	"testNullTrick" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f00 0000 0000 0000 0000 0000 0000 0000 0006'),
+	"Null1.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f00 84ab 5732 baa3 910e cc87 208a de53 a111'),
+	"Null2.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f00 84ab 5732 baa3 910e cc87 208a de53 0003'),
+	"NullTrick.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f00 0000 0000 0000 0000 0000 0000 0000 0006'),
 	
-	"testAdd5Null" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a100 2566 6969 ea'),
-	"testMin1Null" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 00'),
+	"Add5Null.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 a100 2566 6969 ea'),
+	"Min1Null.test" : bytes.fromhex('2b7e 1516 28ae d2a6 abf7 1588 09cf 4f3c 84ab 5732 baa3 910e cc87 208a de53 00'),
 }
 
 for name, content in test_files.items():
---
+++ test_files/test.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+# If number of arguments less then 1; print usage and exit
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 <tested_application>"
+	exit 1
+fi
+
+# Setting the testing environment
+exe="$1" #tested application from args
+tst="diff" #testing application
+test_files=(./*.test) # all test files have the extension of .test
+success=0
+fail=0
+
+for file in "${test_files[@]}"; do
+	# temporary files of encyption and decryption
+	file_enc="$file.enc.tmp"
+	file_dec="$file.dec.tmp"
+
+	echo "Testing $file"
+
+	# encrypt and decrypt the file
+	$exe -e -i $file -o $file_enc -f cipher128
+	$exe -d -i $file_enc -o $file_dec -f cipher128
+	
+	$exe -h -i $file
+	$exe -h -i $file_dec
+	
+
+	# Execute diff
+	$tst $file $file_dec
+
+
+	# use diff exit code to determine success and failure
+	e_code=$?
+	if [ $e_code != 0 ]; then
+		echo TESTING $file FAILED!
+		((fail++))
+	else
+		((success++))
+	fi
+done
+
+echo ========================================
+echo TESTS DONE!
+echo Success: $success
+echo Fail: $fail
+echo ========================================
+
+# Clean up
+rm *.tmp
+
+exit 0
