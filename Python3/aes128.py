9a90481842a7860cf16a7c18ce33f24d433a96d2
U. I. I. Derbashi <32719218+uderbashi@users.noreply.github.com>
2022-10-15 17:05:23 +0000

    finished the python code and passed the tests

--- Python3/aes128.py
+++ Python3/aes128.py
@@ -1,10 +1,76 @@
 from src.args import parse
-from src import aes
-from src import io
+from src.aes import encrypt_block, decrypt_block
+from src import io as IO
 
 def main():
 	args = parse()
-	io.load_io(args)
+	io = IO.load_io(args)
+
+	if args.encrypt:
+		encrypt_file(io)
+	elif args.decrypt:
+		decrypt_file(io)
+	else: # args.hash
+		hash_file(io)
+
+	IO.unload_io(io)
+
+def encrypt_file(io):
+	block, length = IO.read_block(io.input)
+
+	while length > 0:
+		block = encrypt_block(block, io.keys)
+		io.output.write(bytearray(block))
+
+		if length == 15:
+			block = [0 for x in range(15)] + [17]
+			length = 16
+			continue
+
+		block, length = IO.read_block(io.input)
+
+def decrypt_file(io):
+	runs = io.input_size // 16
+	flag = False #flag that is raised when the last byte of the penutimate block is 0
+
+	for i in range(runs):
+		block, _ = IO.read_block(io.input)
+		block = decrypt_block(block, io.keys)
+
+		if i == runs - 2 and block[-1] == 0:
+			flag = True
+			io.output.write(bytearray(block[:15]))
+		elif i != runs - 1:
+			io.output.write(bytearray(block))
+		elif IO.all_zeroes(block):
+			if block[-1] == 17:
+				if not flag:
+					io.output.write(bytearray(block))
+			else:
+				if flag:
+					io.output.write(bytearray([0]))
+				io.output.write(bytearray(block[:-block[-1]]))
+		else:
+			if flag:
+				io.output.write(bytearray([0]))
+			io.output.write(bytearray(block))
+
+def hash_file(io):
+	out = [0 for x in range(16)]
+	block, length = IO.read_block(io.input)
+
+	while length > 0:
+		block = encrypt_block(block, io.keys)
+		out = [x^y for x,y in zip(out, block)]
+
+		if length == 15:
+			block = [0 for x in range(15)] + [17]
+			length = 16
+			continue
+
+		block, length = IO.read_block(io.input)
+
+	print(bytearray(out).hex())
 
 if __name__ == "__main__":
 	main()
--- Python3/src/io.py
+++ Python3/src/io.py
@@ -1,3 +1,7 @@
+from os.path import getsize
+from src.aes import init_keys
+from sys import exit
+
 class io_t:
 	'''
 	Will include file pointers to input and output and the key
@@ -8,9 +12,11 @@ def load_io(args):
 	io = io_t()
 	try:
 		io.input = open(args.i, 'rb')
+		io.input_size = getsize(args.i)
 		if args.hash:
+			io.output = None
 			key = bytearray.fromhex("3141 5926 5358 9793 2384 6264 3383 2795")
-			io.key = [x for x in key]
+			key = [x for x in key]
 
 		else: #if not args.hash
 			io.output = open(args.o, 'wb')
@@ -18,9 +24,40 @@ def load_io(args):
 			if args.file_key is not None:
 				with open(args.file_key, 'rb') as fk:
 					key = fk.read(16)
-				io.key = [x for x in key]
+				key = [x for x in key]
 			else: # if args.key is not None
-				io.key = [ord(x) for x in args.key]
-
+				key = [ord(x) for x in args.key]
 	except:
 		print("Issues with files")
+		exit(-1)
+
+	io.keys = init_keys(key)
+	return io
+
+def unload_io(io):
+	io.input.close()
+	if io.output is not None:
+		io.output.close()
+
+
+def read_block(io_file):
+	block = [x for x in io_file.read(16)]
+	l = len(block)
+
+	if l < 15:
+		block += [0 for i in range(16 - l - 1)]
+		block += [16 - l]
+	elif l == 15:
+		block += [0]
+
+	return block, l
+
+def all_zeroes(block):
+	pad = block[-1]
+	if pad < 16 and pad > 1:
+		return all(x == 0 for x in block[-pad:15])
+
+	if pad == 17:
+		return all(x == 0 for x in block[:15])
+
+	return False
