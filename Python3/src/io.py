50b4722db33d35977236067fd93890b712ac7d88
U. I. I. Derbashi <32719218+uderbashi@users.noreply.github.com>
2022-10-15 14:12:07 +0000

    remembered that hash actually doesn't require a ey and added its own (pi)

--- Python3/src/io.py
+++ Python3/src/io.py
@@ -8,16 +8,19 @@ def load_io(args):
 	io = io_t()
 	try:
 		io.input = open(args.i, 'rb')
-		if not args.hash:
+		if args.hash:
+			key = bytearray.fromhex("3141 5926 5358 9793 2384 6264 3383 2795")
+			io.key = [x for x in key]
+
+		else: #if not args.hash
 			io.output = open(args.o, 'wb')
 
-		if args.file_key is not None:
-			with open(args.file_key, 'rb') as fk:
-				key = fk.read(16)
-			io.key = [x for x in key]
-		else: # if args.key is not None
-			io.key = [ord(x) for x in args.key]
-
+			if args.file_key is not None:
+				with open(args.file_key, 'rb') as fk:
+					key = fk.read(16)
+				io.key = [x for x in key]
+			else: # if args.key is not None
+				io.key = [ord(x) for x in args.key]
 
 	except:
 		print("Issues with files")
