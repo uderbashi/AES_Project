95ab6547202702b356dbe5826cef35b7eae1dca5
U. I. I. Derbashi <32719218+uderbashi@users.noreply.github.com>
2023-01-05 16:23:49 +0000

    finished the main file

--- Lua/aes128.lua
+++ Lua/aes128.lua
@@ -1,5 +1,18 @@
 #!/usr/bin/env lua
+function main ()
+	local aes = require("src/aes")
+	local aes_io = require("src/aes_io")
+	local args = require("src/args")
 
-AESMatrix = require("src/aes_matrix")
-a = AESMatrix:new()
-print(a)
+	args.parse(arg)
+	args = args.args
+	aes_io.load_io(args)
+	if args.act == "e" then aes.encrypt(aes_io)
+	elseif args.act == "d" then aes.decrypt(aes_io)
+	else aes.hash(aes_io) end
+
+	aes_io.unload_io()
+
+	return 0
+end
+main()
